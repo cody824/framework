fac56d0fac1b965c282fa7f9374f5affe7c41eb5
guodong <cody.guo@soulinfo.com>
2020-07-22 12:18:57 +0800

    add CustomAjaxLoginUrlAuthenticationEntryPoint

---
+++ src/main/java/com/noknown/framework/security/web/authentication/CustomAjaxLoginUrlAuthenticationEntryPoint.java
@@ -0,0 +1,118 @@
+package com.noknown.framework.security.web.authentication;
+
+import com.noknown.framework.common.util.JsonUtil;
+import com.noknown.framework.common.web.model.ErrorMsg;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author guodong
+ */
+public class CustomAjaxLoginUrlAuthenticationEntryPoint extends LoginUrlAuthenticationEntryPoint{
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	private final boolean response401;
+
+	private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+	public CustomAjaxLoginUrlAuthenticationEntryPoint(String defaultView, boolean response401){
+		super(defaultView);
+		this.response401 = response401;
+	}
+
+	/**
+	 * Performs the redirect (or forward) to the login form URL.
+	 */
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+			throws IOException, ServletException {
+
+		String redirectUrl = null;
+
+		boolean isAjax = isAjaxRequest(request);
+		if (isAjax) {
+			if (response401) {
+				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			}
+			ErrorMsg msg = new ErrorMsg();
+			msg.setErrorNum(401);
+			msg.setErrorMsg("对不起，您需要登录");
+			responseOutWithJson(response, msg);
+			return;
+		}
+
+		if (isUseForward()) {
+
+			if (isForceHttps() && "http".equals(request.getScheme())) {
+				// First redirect the current request to HTTPS.
+				// When that request is received, the forward to the login page will be used.
+				redirectUrl = buildHttpsRedirectUrlForRequest(request);
+			}
+
+			if (redirectUrl == null) {
+				String loginForm = determineUrlToUseForThisRequest(request, response, authException);
+
+				if (logger.isDebugEnabled()) {
+					logger.debug("Server side forward to: " + loginForm);
+				}
+
+				RequestDispatcher dispatcher = request.getRequestDispatcher(loginForm);
+
+				dispatcher.forward(request, response);
+
+				return;
+			}
+		} else {
+			// redirect to login page. Use https if forceHttps true
+
+			redirectUrl = buildRedirectUrlToLoginPage(request, response, authException);
+
+		}
+
+		redirectStrategy.sendRedirect(request, response, redirectUrl);
+	}
+
+
+	private boolean isAjaxRequest(HttpServletRequest request) {
+		boolean check = false;
+		if ("XMLHttpRequest".equals(request.getHeader("X-Requested-With"))) {
+			check = true;
+		} else if ("application/json".equalsIgnoreCase(request.getHeader("Accept"))) {
+			check = true;
+		}
+		return check;
+	}
+
+	protected void responseOutWithJson(HttpServletResponse response, Object responseObject) {
+		// 将实体对象转换为JSON Object转换
+		String json = JsonUtil.toJson(responseObject);
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json; charset=utf-8");
+		PrintWriter out;
+		try {
+			out = response.getWriter();
+			out.append(json);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void setUseForward(@Value("${security.login.userForward:true}") boolean useForward) {
+		super.setUseForward(useForward);
+	}
+}
