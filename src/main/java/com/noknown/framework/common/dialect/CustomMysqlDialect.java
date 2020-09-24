d5fc7a2454ceb9c658da5d73d50afb9f5a7fc867
guodong <cody.guo@soulinfo.com>
2020-09-24 22:10:34 +0800

    修改数据库方言，基于MySQL57Dialect

--- src/main/java/com/noknown/framework/common/dialect/CustomMysqlDialect.java
+++ src/main/java/com/noknown/framework/common/dialect/CustomMysqlDialect.java
@@ -1,15 +1,16 @@
 package com.noknown.framework.common.dialect;
 
 import com.noknown.framework.common.hql.BitAndFunction;
-import org.hibernate.dialect.MySQL5Dialect;
+import org.hibernate.dialect.*;
 
 /**
  * @author guodong
  */
-public class CustomMysqlDialect extends MySQL5Dialect {
+public class CustomMysqlDialect extends MySQL57Dialect {
 
 	public CustomMysqlDialect() {
 		super();
 		this.registerFunction("bitand", new BitAndFunction());
 	}
+
 }
